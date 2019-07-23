68e1cf280eae4d41327844ba3bae2ea7ce206783
tran <tran@travelsky.com>
2019-07-23 17:04:46 +0800

    新增登陆方法

--- api/src/main/java/com/ginko/driver/api/moudle/login/UserController.java
+++ api/src/main/java/com/ginko/driver/api/moudle/login/UserController.java
@@ -1,6 +1,5 @@
 package com.ginko.driver.api.moudle.login;
 
-
 import com.ginko.driver.common.entity.MsgConfig;
 import com.ginko.driver.common.exception.MsgEnum;
 import com.ginko.driver.framework.entity.SysUser;
@@ -11,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @Author: tran
+ * @Description:
+ * @Date Create in 14:45 2019/7/22
+ */
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -18,15 +22,27 @@ public class UserController {
     @Autowired
     private SysUserService sysUserService;
 
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public MsgConfig login(@RequestBody SysUser sysUser) {
+        SysUser sysUser1 = sysUserService.findByuserName(sysUser.getUserName());
+        if (sysUser1 == null) {
+            return new MsgConfig("ERROR", MsgEnum.USERNOTEXIST.getDesc());
+        }
+        /*用户名或密码错误*/
+        if (!sysUser1.getPassword().equals(sysUser.getPassword())) {
+            return new MsgConfig("ERROR", MsgEnum.USERNAMEANDPASSWORDERROR.getDesc());
+        }
+        return new MsgConfig("SUCCESS", "", sysUser1);
+    }
 
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public MsgConfig login(@RequestBody SysUser data){
-       SysUser sysUser = sysUserService.findByuserNameAndPassword(data.getUserName(),data.getPassword());
 
-       if (sysUser==null){
-           return new MsgConfig("ERROR",MsgEnum.USERNAMEANDPASSWORDERROR.getDesc());
-       }
-
-        return new MsgConfig("SUCCESS","",sysUser);
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public MsgConfig register(@RequestBody SysUser sysUser) {
+        SysUser sysUser1 = sysUserService.findByuserName(sysUser.getUserName());
+        if (sysUser1 != null) {
+            return new MsgConfig("ERROR", MsgEnum.USERALREADYEXIST.getDesc());
+        }
+        sysUserService.saveUser(sysUser);
+        return new MsgConfig("SUCCESS", "", null);
     }
 }
