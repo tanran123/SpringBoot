7564d7b6e207f3ccc8dfb4424857127fa761b782
tran <tran@travelsky.com>
2019-07-31 16:52:16 +0800

    mongoDB相关

--- api/src/main/java/com/ginko/driver/api/moudle/TestController.java
+++ api/src/main/java/com/ginko/driver/api/moudle/TestController.java
@@ -9,10 +9,7 @@ import com.ginko.driver.framework.entity.PxEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -28,18 +25,49 @@ public class TestController {
     @Autowired
     private MongoPxDaoImp mongoPxDaoImp;
 
-    @RequestMapping(value = "/getUserInfo",method = RequestMethod.POST)
-    public MsgConfig getUserInfo(){
-       return new MsgConfig("200","返回成功",200);
+    @RequestMapping(value = "/getUserInfo", method = RequestMethod.POST)
+    public MsgConfig getUserInfo() {
+        return new MsgConfig("200", "返回成功", 200);
     }
 
 
-    @RequestMapping(value = "/getMongo",method = RequestMethod.POST)
-    public MsgConfig getMongo(){
+    @RequestMapping(value = "/getMongo", method = RequestMethod.POST)
+    public MsgConfig getMongo() {
         logger.info("-----------------请求像素数据-----------------");
         PxEntity pxEntity = new PxEntity();
         pxEntity.setUserId(0);
-        List<PxEntity> boxEntities = mongoPxDaoImp.getPage(pxEntity,0,360000);
-        return new MsgConfig("200",String.valueOf(boxEntities.size()),boxEntities);
+        List<PxEntity> boxEntities = mongoPxDaoImp.getPage(pxEntity, 0, 360000);
+        return new MsgConfig("200", String.valueOf(boxEntities.size()), boxEntities);
+    }
+
+    @RequestMapping(value = "/updatePx", method = RequestMethod.POST)
+    public MsgConfig updatePx(@RequestBody PxEntity pxEntityP) {
+        logger.info("-----------------修改像素数据-----------------x:" + pxEntityP.getX() + "y:" + pxEntityP.getY());
+        PxEntity pxEntity = new PxEntity();
+        pxEntity.setX(pxEntityP.getX());
+        pxEntity.setY(pxEntityP.getY());
+        List<PxEntity> pxEntities = mongoPxDaoImp.queryList(pxEntity);
+        if (pxEntities.size() > 0) {
+          /*  if (pxEntityP.getUserId() != pxEntities.get(0).getUserId()) {
+                return new MsgConfig("401", "这块地属于别的大佬", null);
+            }*/
+            //存在则修改像素信息
+            pxEntity.setR(pxEntityP.getR());
+            pxEntity.setG(pxEntityP.getG());
+            pxEntity.setB(pxEntityP.getB());
+            pxEntity.setA(pxEntityP.getA());
+            mongoPxDaoImp.updateFirst(pxEntities.get(0), pxEntity);
+        }
+        //不存在则新增像素信息
+        else {
+            pxEntity.setUserId(pxEntityP.getUserId());
+            pxEntity.setR(pxEntityP.getR());
+            pxEntity.setG(pxEntityP.getG());
+            pxEntity.setB(pxEntityP.getB());
+            pxEntity.setA(pxEntityP.getA());
+            mongoPxDaoImp.save(pxEntity);
+        }
+        System.out.println(pxEntities.size());
+        return new MsgConfig("200", "修改成功", pxEntity);
     }
 }
